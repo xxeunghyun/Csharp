3b15e054b2bf8576cd7a7c30dad3a378eb4ccaee
seunghyunmin <68652374+xxeunghyun@users.noreply.github.com>
2020-09-03 22:53:00 +0900

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1
+{
+    public partial class Form1 : Form
+    {
+        CheckBox[] chkbx;
+        const int SIZE = 6;
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            chkbx = new CheckBox[SIZE];
+
+            // 0~size - 1    checkbox 1~size
+            chkbx[0] = checkBox1;
+            chkbx[1] = checkBox2;
+            chkbx[2] = checkBox3;
+            chkbx[3] = checkBox4;
+            chkbx[4] = checkBox5;
+            chkbx[5] = checkBox6;
+        }
+
+        private void checkBox6_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void a_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //선택완료 버튼
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear(); //안에 내용 비우기
+
+            int i;
+
+            for(i=0;i<SIZE;i++)
+            {
+                if(chkbx[i].Checked == true)
+                {
+                    listBox2.Items.Add(chkbx[i].Text);
+                }
+            }
+            /*
+             if (checkBox1.Checked == true)
+                  listBox2.Items.Add(checkBox1.Text);
+
+              if (checkBox2.Checked == true)
+                  listBox2.Items.Add(checkBox2.Text);
+
+              if (checkBox3.Checked == true)
+                  listBox2.Items.Add(checkBox3.Text);
+
+              if (checkBox4.Checked == true)
+                  listBox2.Items.Add(checkBox4.Text);
+
+              if (checkBox5.Checked == true)
+                  listBox2.Items.Add(checkBox5.Text);
+
+              if (checkBox6.Checked == true)
+                  listBox2.Items.Add(checkBox6.Text);
+                  */
+        }
+    }
+}
