2c0a9f1d5acda86c7073892c2876cdb9fdfa8047
seunghyunmin <68652374+xxeunghyun@users.noreply.github.com>
2020-09-03 22:59:36 +0900

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBox1.SelectedIndex == 0)
+            {
+                label1.Image = Properties.Resources.자동차__1_;
+            }
+
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                label1.Image = Properties.Resources.자동차__2_;
+            }
+
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                label1.Image = Properties.Resources.자동차__3_;
+            }
+
+            else if (comboBox1.SelectedIndex == 3)
+            {
+                label1.Image = Properties.Resources.자동차__4_;
+            }
+
+            else if (comboBox1.SelectedIndex == 4)
+            {
+                label1.Image = Properties.Resources.자동차__5_;
+            }
+        }
+    }
+}
